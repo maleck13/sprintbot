46a1799e4b9b84f41b3aa2102f82a830529fc50a
craig <cbrookes@redhat.com>
2017-06-14 22:50:19 +0100

    increase sync time

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -99,13 +99,13 @@ func main() {
 	//start sync
 	{
 
-		go sprintService.ScheduleSync(20*time.Second, shutDownChan)
+		go sprintService.ScheduleSync(30*time.Second, shutDownChan)
 	}
 
 	//http handler
 	{
 		port := ":3000"
-		logrus.Info("starting sprintbot 0.0.1 on  port " + port)
+		logrus.Info("starting sprintbot 0.0.1 	on  port " + port)
 		httpHandler := web.BuildHTTPHandler(router)
 		if err := http.ListenAndServe(port, httpHandler); err != nil {
 			logger.Fatal(err)
